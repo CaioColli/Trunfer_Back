ecdf63cf35165fc51dbddfca1d2ac87c799ccaf7
Caio Colli <caiocolli22@gmail.com>
2024-12-04 15:08:57 -0300

    Primeiro commit

---
+++ src/app/App.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+class App
+{
+    /**
+     * Inicia a execução do framework, declara os middlewares, handlers, tipo de acesso e chama o registro de rotas.
+     */
+    public static function run()
+    {
+
+        $app = AppFactory::create();
+
+        // Rota para a raiz do projeto ("/")
+        $app->get('/', function (Request $request, Response $response) {
+            $response->getBody()->write("Página inicial");
+            return $response;
+        });
+
+        $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+            $name = $args['name'];
+            $response->getBody()->write("Hello, $name");
+            return $response;
+        });
+        
+        $app->run();
+    }
+}
